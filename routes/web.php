6ca0d2cf60cb0777f985ca12d3e7bd135bbf5341
barbapapazes <e.soubiran25@gmail.com>
2025-09-07 22:59:50 +0200

    fix: inertia to view

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia('index', [
+    return view('index', [
         'time' => now()->toDateTimeString(),
         'event' => 'HumanTalks Paris',
     ]);
