cfadf7ceff4c44b061a57e91efe2d5c70844e49d
barbapapazes <e.soubiran25@gmail.com>
2025-09-09 00:26:40 +0200

    fix: missing register

---
+++ resources/js/pages/register.vue
@@ -0,0 +1,9 @@
+<template>
+  <form method="post" action="/register">
+    <input name="email" type="email" placeholder="Email" />
+
+    <input name="password" type="password" placeholder="Password" />
+
+    <button type="submit">Register</button>
+  </form>
+</template>
